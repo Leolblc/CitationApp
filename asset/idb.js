3f7d959966d77583200ebf8db0aa8fa30a37a0e1
Leo LEBLANC <leo.leblanc@lyceefulbert.fr>
2024-11-25 15:28:48 +0100

    First Commit

--- asset/idb.js
+++ asset/idb.js
@@ -23,4 +23,50 @@ window.addEventListener("load", async (event) => {
     }
 
     const db = await initDB();
+
+
+async function addCitation(citation) {
+        const transaction = db.transaction("citation", "readwrite");
+        const store = transaction.objectStore("citation");
+        await store.add(citation);
+}
+
+async function getCitations() {
+    return new Promise((resolve, reject) => {
+        const transaction = db.transaction("citation", "readonly");
+        const store = transaction.objectStore("citation");
+        const request = store.getAll();
+
+        request.onsuccess = (event) => {
+            console.log("Citations récupérées :", request.result);
+            resolve(request.result);
+        };
+
+        request.onerror = (event) => {
+            reject("Erreur lors de la récupération des citations.");
+        };
+    });
+}
+
+async function modelFetchCitation(){
+    try{
+        citation = await getCitations();
+
+        if(citation.length === 0){
+            citation = await fetch('api1/index.php')
+            .then(response => response.json())
+            .then(data =>{
+                data.forEach(async(citation)=>{
+                    await addCitation(citation);
+                });
+                return data
+            })
+            .catch(error => console.error('Error fetching citation', error))
+        }
+    } catch(error){
+        console.error(error);
+    }
+}
+
+modelFetchCitation();
 });
